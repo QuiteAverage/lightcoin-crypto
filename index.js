973959d03d73681a380a3eb49091d120f756d01a
Jamie Li <lijamie19828@gmail.com>
2021-11-06 00:01:34 +0000

    wrote isAllowed into deposit, withdrawal and commit . wrote get balance, wrote wrote addTransactions

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ let balance = 500.00;
 class Account {
   constructor(username){
     this.username = username;
-    this.balance = 0;
+    this.transactions = [];
+  }
+
+  get balance() {
+    let balance = 0;
+    for (let i = 0; i < this.transactions.length; i++) {
+      balance += this.transactions[i].value;
+    }
+    return balance;
+  }
+
+  addTransactions(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
@@ -13,23 +25,42 @@ class Transaction {
     this.amount = amount;
     this.account = account;
   }
-}
-class Withdrawal extends Transaction {
 
   commit() {
-    this.account.balance -= this.amount;
+    if(this.isAllowed()) {
+      this.account.addTransactions(this);
+      return true
+    }
+    return false
   }
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount
+  }
+
+  isAllowed() {
+    if (this.account.balance - this.amount >= 0) {
+      return true
+    }
+  }
+
 
 }
 
 class Deposit extends Transaction {
 
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount
   }
+
+  isAllowed() {
+    return true
+  }
+
 }
-
-
 
 
 // DRIVER CODE BELOW
@@ -37,18 +68,26 @@ class Deposit extends Transaction {
 
 const myAccount = new Account("snow-patrol")
 
+// Should Fail. false balance 0
 t1 = new Withdrawal(50.25, myAccount);
-t1.commit();
-console.log('Transaction 1:', t1);
+console.log(t1.commit());
+console.log('Account Balance', myAccount.balance);
 
-t2 = new Withdrawal(9.99, myAccount);
-t2.commit();
-console.log('Transaction 2:', t2);
+// Should Pass. true balance 120
+t2 = new Deposit(120.00, myAccount);
+console.log(t2.commit());
+console.log('Account Balance', myAccount.balance);
 
-t3 = new Deposit(120.00, myAccount);
-t3.commit()
-console.log('Transaction 3:', t3);
+// Should Pass. true balance 110.01
+t3 = new Withdrawal(9.99, myAccount);
+console.log(t3.commit());
+console.log('Account Balance', myAccount.balance);
 
-console.log('Balance:', myAccount.balance);
+// Should Fail. false balance 110.01
+t4 = new Withdrawal(110.02, myAccount);
+console.log(t4.commit());
+console.log('Account Balance', myAccount.balance);
+
+console.log('Transactions:', myAccount.transactions);
 
 
